bf048d38283155bc8bc125ac1f953b45e993211f
Anindita Basu <ab.techwriter@gmail.com>
2022-07-22 18:18:06 +0530

    gh-pages test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,11 +14,16 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+
 # -- Project information -----------------------------------------------------
 
-project = 'Writing Technically'
+project = 'Sphinx demo site'
 copyright = '2022, Anindita Basu'
 author = 'Anindita Basu'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0'
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -26,16 +31,7 @@ author = 'Anindita Basu'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
 ]
-
-#myst_parser extensions
-
-myst_enable_extensions = [
-    "html_admonition",
-]
-
-myst_heading_anchors = 3
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -43,18 +39,17 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
+
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'haiku'
+html_theme = 'pyramid'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_title = 'Demo Sphinx site'
-html_last_updated_fmt = '%d %B %Y'
---
+++ docs/source/index.rst
@@ -0,0 +1,13 @@
+.. You can change this file to contain whatever you want, but it should always contain the root `toctree` directive.
+
+the Sphinx demo site
+======================
+
+A message is a verbal, written, or recorded communication that's sent to (or left for) someone who can't be contacted directly.
+
+.. toctree::
+   :maxdepth: 1
+
+   concept
+   task
+
